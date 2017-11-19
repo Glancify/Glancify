4f9ae73cf007b1b5e01e71962891876a18ab5305
simsausaurabh <saurabhsri.sri123@gmail.com>
2017-11-19 17:56:45 +0530

    add UI new tab

--- Extension/Data/twitter/twitter.js
+++ Extension/Data/twitter/twitter.js
@@ -1,15 +1,34 @@
 var htmldata = ''
+var no_of_li=0;
 
-$(function() {
+jQuery(function() {
 	$.get('https://twitter.com/i/notifications', function(data) {
 		htmldata = ''+data;
-
+		//console.log(data);
 		$('body').append($(htmldata).find('#stream-items-id').eq(0));
+		//console.log(find('#stream-items-id'));
+		// window.no_of_li = $("#stream-items-id > li").size();
+		// alert(no_of_li);	
 
-	})
+
+
+
+		
+	});
 });
 
-function printNotification() {
+ 
+// for( i = 1; i <= no_of_li; i++ ) {
 
-	document.write(data);
-}
+// 	var newDiv = document.createElement('div');
+// 	document.body.appendChild(newDiv);
+// 	newDiv.style.width = "500px";
+// 	newDiv.style.height = "500px";
+// 	newDiv.style.background = "#77a4d4";
+// 	newDiv.style.borderStyle = "solid";
+// 	newDiv.style.borderWidth = "1px";
+// 	newDiv.style.color = "blue";
+
+// 	alert("alok");
+
+// }
